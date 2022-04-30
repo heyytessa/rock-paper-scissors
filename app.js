df734c356bde3ef48269303e6083e56cd7739494
Tessa <heyytessa@gmail.com>
2022-04-30 16:02:13 -0700

    basic rock paper scissors game

---
+++ app.js
@@ -0,0 +1,69 @@
+let gamePlay = ['rock', 'paper', 'scissors'];
+let playerScore = 0
+let computerScore = 0
+
+let winnerAnnouncement = document.querySelector('div');
+console.log(winnerAnnouncement);
+let winner = document.createElement('p');
+console.log(winner);
+winnerAnnouncement.appendChild(winner);
+
+function computerPlay() {
+  let computerSelection = gamePlay[Math.floor(Math.random()*gamePlay.length)];
+  return computerSelection;
+}
+
+function playRound(playerSelection, computerSelection) {  
+  if (playerSelection === computerSelection) {
+  } else if (playerSelection === 'rock') {
+    switch (computerSelection) {
+      case 'paper':
+        computerScore++;
+        break;
+      case 'scissors':
+        playerScore++;
+      break;
+    }
+  } else if (playerSelection === 'paper') {
+    switch (computerSelection) {
+      case 'rock':
+        playerScore++;
+        break;
+      case 'scissors':
+        computerScore++;
+        break;
+    }
+  } else if (playerSelection == 'scissors') {
+    switch (computerSelection) {
+      case 'rock':
+        computerScore++
+        break;
+      case 'paper':
+        playerScore++
+        break;
+    }
+  }
+  console.log(playerScore);
+  console.log(computerScore);
+}
+
+function game() {
+  for (i=0; i<5; i++) {
+  let playerSelection = prompt('Select rock, paper, or scissors');
+  let computerSelection = computerPlay();
+  playRound(playerSelection, computerSelection);
+  }
+
+  if (playerScore > computerScore) {
+    winner.textContent = 'Congratulations, you won!';
+  } else if (playerScore < computerScore) {
+    winner.textContent = 'Sorry, you lost!';
+  } else {
+    winner.textContent = 'That\'s a draw!';
+  }
+
+
+}
+
+game();
+
